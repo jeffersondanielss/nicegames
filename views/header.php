8f2fdcc05d35533dea91fd85ab6a70d9ff2681ff
Jefferson Daniel <jeffersondanielss@gmail.com>
2017-05-18 19:35:50 -0300

    alt attribute

--- views/header.php
+++ views/header.php
@@ -16,7 +16,7 @@
     <div class="row">
       <div class="col-sm-6">
         <div class="logo">
-          <h1><a href="./"><img src="img/ng-logo.png"></a></h1>
+          <h1><a href="./"><img src="img/ng-logo.png"  alt="nicegames"></a></h1>
         </div>
       </div>
 
