8aad58ede62cf7d945e109f5e7275688b67c9e28
Jefferson Daniel <jeffersondanielss@gmail.com>
2017-06-15 11:45:46 -0300

    menu

--- dashboard/views/sidebar.php
+++ dashboard/views/sidebar.php
@@ -1,6 +1,8 @@
 <div class="collapse navbar-collapse navbar-ex1-collapse">
   <ul class="nav navbar-nav side-nav">
     <li><a href="./"><i class="fa fa-fw fa-dashboard"></i> Painel</a></li>
+    <li><a href="client.php"><i class="glyphicon glyphicon-user"></i> Clientes</a></li>
+    <li><a href="support.php"><i class="fa fa-fw fa-life-ring"></i> Suporte</a></li>
 
     <li>
       <a data-toggle="collapse" data-target="#product">
@@ -14,18 +16,6 @@
     </li>
 
     <li>
-      <a data-toggle="collapse" data-target="#client">
-        <i class="glyphicon glyphicon-user"></i> Clientes 
-        <i class="fa fa-fw fa-caret-down pull-right"></i>
-      </a>
-
-      <ul id="client" class="collapse sub-collapse">
-        <!-- <li><a href="views/client/cadastro.php">Cadastro</a></li> -->
-        <li><a href="client.php">Ver todos</a></li>
-      </ul>
-    </li>
-
-    <li>
       <a data-toggle="collapse" data-target="#employe">
         <i class="glyphicon glyphicon-user"></i> Funcionários 
         <i class="fa fa-fw fa-caret-down pull-right"></i>
@@ -35,8 +25,6 @@
         <li><a href="employe.php">Ver todos</a></li>
       </ul>
     </li>
-
-    <li><a href="support.php"><i class="fa fa-fw fa-life-ring"></i> Suporte</a></li>
 
   </ul>
 </div>
